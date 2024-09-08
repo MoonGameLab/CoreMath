10a104cdf9e2d355d0eff074269db769fa432b5b
Tourahi <amine-fb-fb@hotmail.fr>
2024-09-08 21:53:05 +0100

    Init project.

---
+++ CoreMath/Vector2D.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMath
+{
+    /// <summary>
+    /// 2D Vector class.
+    /// </summary>
+    public class Vector2D
+    {
+        
+    }
+}
