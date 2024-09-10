4ad4d027196085d2c55af2cbdd3ddf0790d6c2b7
Tourahi <amine-fb-fb@hotmail.fr>
2024-09-10 01:22:16 +0100

    Assemby Info & Vector2D static props.

--- CoreMath/Vector2D.cs
+++ CoreMath/Vector2D.cs
@@ -1,15 +1,71 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace CoreMath
+#if SIMD
+using Mono.Simd;
+#endif
+
+namespace MGL.CoreMath
 {
     /// <summary>
     /// 2D Vector class.
     /// </summary>
-    public class Vector2D
+    [Serializable]
+    public struct Vector2D : IEquatable<Vector2D>
     {
-        
+        float x, y;
+
+        public float X { get { return x; } }
+        public float Y { get { return y; } }
+
+        public Vector2D(float value)
+        {
+            x = y = value;
+        }
+
+        public Vector2D(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        #region Static properties
+
+        public static Vector2D UnitX {
+            get {
+                return new Vector2D(1f, 0f);
+            }
+        }
+
+        public static Vector2D UnitY {
+            get {
+                return new Vector2D(0f, 1f);
+            }
+        }
+
+        public static Vector2D One {
+            get {
+                return new Vector2D(1f);
+            }
+        }
+
+        public static Vector2D NgOne {
+            get {
+                return new Vector2D(-1f);
+            }
+        }
+
+        public static Vector2D Zero {
+            get {
+                return new Vector2D(0f);
+            }
+        }
+
+        #endregion
+
+
+        public bool Equals(Vector2D other)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
