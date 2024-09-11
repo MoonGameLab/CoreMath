03b296d666c4dc7b9df1ac020a872d14c510362a
Tourahi <amine-fb-fb@hotmail.fr>
2024-09-11 04:35:59 +0100

    Equality & Div<Arith>.

--- CoreMath/Vector2D.cs
+++ CoreMath/Vector2D.cs
@@ -143,13 +143,86 @@ namespace MGL.CoreMath
       return v;
     }
 
+    public static Vector2D Div (Vector2D v, float divider)
+    {
+#if SIMD
+      var v4 = new Vector4f (v.x, v.y, 0f, 0f) / new Vector4f (divider);
+			return new Vector2 (v4.X, v4.Y);
+#else
+      return new Vector2D (v.x / divider, v.y / divider);
+#endif
+    }
+
+    public static void Div (ref Vector2D v, float divider, out Vector2D res)
+    {
+#if SIMD
+      var v4 = new Vector4f (v.x, v.y, 0f, 0f) / new Vector4f (divider);
+			res.x = v4.X;
+			res.y = v4.Y;
+#else
+      res.x = v.x / divider;
+			res.y = v.y / divider;
+#endif     
+    }
+
+    public static Vector2D Div (Vector2D v1, Vector2D v2)
+    {
+#if SIMD
+      var v4 = new Vector4f (v1.x, v1.y, 0f, 0f) / new Vector4f (v2.x, v2.y, 0f, 0f);
+      return new Vector2 (v4.X, v4.Y);
+#else
+      return new Vector2D (v1.x / v2.x, v1.y / v2.x);
+#endif
+    }
+
+    public static void Div (ref Vector2D v1, ref Vector2D v2, out Vector2D res)
+    {
+#if SIMD
+      var v4 = new Vector4f (v1.x, v1.y, 0f, 0f) / new Vector4f (v2.x, v2.y, 0f, 0f);
+      res.x = v4.X;
+      res.y = v4.Y;
+#else
+      res.x = v1.x * v2.x;
+			res.y = v1.y * v2.x;
+#endif    
+    }
+
     #endregion
 
 
+    #region Equality
 
     public bool Equals(Vector2D other)
     {
-      throw new NotImplementedException();
+      return x == other.x && y == other.y;
     }
+  
+    public override bool Equals (object? obj)
+		{
+			return obj is Vector2D && ((Vector2D)obj) == this;
+		}
+
+    public static bool operator == (Vector2D a, Vector2D b)
+		{
+			return a.x == b.x && a.y == b.y;
+		}
+
+    public static bool operator != (Vector2D a, Vector2D b)
+		{
+			return a.x != b.x || a.y != b.y;
+		}
+
+    public override int GetHashCode ()
+		{
+			return x.GetHashCode () ^ y.GetHashCode ();
+		}
+
+    #endregion
+
+    public override string ToString ()
+		{
+			return string.Format ("{{X:{0} Y:{1}}}", X, Y);
+		}
+
   }
 }
